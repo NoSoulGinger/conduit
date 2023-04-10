5fe6f23c54e9df722b2df3a2906042862a3f8d3c
Scheib Ákos <schaki9518@gmail.com>
2023-04-10 12:28:01 +0200

    Uploading registration test

---
+++ conduit_tests/test_registration.py
@@ -0,0 +1,27 @@
+import time
+from data_for_testing import user
+from main_functions import registration
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+
+class TestLogin(object):
+    def setup_method(self):
+        service = Service(executable_path=ChromeDriverManager().install())
+        options = Options()
+        options.add_experimental_option("detach", True)
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        self.browser = webdriver.Chrome(service=service, options=options)
+        url = 'http://localhost:1667/#/'
+        self.browser.get(url)
+        self.browser.maximize_window()
+
+    def teardown_method(self):
+        self.browser.quit()
+
+    def test_registration_valid_data(self):
+        registration(self.browser, user["name"], user["email"], user["password"])
