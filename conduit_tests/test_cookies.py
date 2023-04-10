834aec01490e4c3c689791a932518ddeee548acb
Scheib Ákos <schaki9518@gmail.com>
2023-04-10 19:21:55 +0200

    Cookie test "reforged"

--- conduit_tests/test_cookies.py
+++ conduit_tests/test_cookies.py
@@ -12,9 +12,9 @@ class TestCookies(object):
         service = Service(executable_path=ChromeDriverManager().install())
         options = Options()
         options.add_experimental_option("detach", True)
-        """options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')"""
+        options.add_argument('--disable-dev-shm-usage')
         self.browser = webdriver.Chrome(service=service, options=options)
         url = 'http://localhost:1667/#/'
         self.browser.get(url)
